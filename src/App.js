637d0d363d1521e9f42f56f352d7aa15acda6336
firas bouhamda <firasbouhamda@gmail.com>
2021-01-27 15:37:19 +0100

    f commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import react from 'react';
 import './App.css';
 import { stayel } from "./stayel.jsx";
-import { javascript } from './javascript.png';
-import { nod } from "./nod.png";
+import  java  from './javascript.png';
 function App() {
   return (
     <div className="App">
       <div style={{border:('1px'),blackMaxWidth:'100vw'}}/>
 <h1 className="title red">Firas Bouhamda</h1>
 <br/ >
-<img src="/src/javascript.png" alt='javascript.png'/>
-<img src="/src/nod.png" alt="nod.png"/>
+<img src={java} alt='javascript' width={400}/>
+<img src="/nod.png" width={600}/>
 <br/>
       <video width="320" height="240" controls >
       <source src="..Videos/video1.mp4" type="video/mp4"/> </video>
